06c534b3134783be28b898973676d2a7bad44af6
JTRMinsk <jtrminsk@yahoo.com>
2022-02-13 16:40:46 +0800

    2022 2/13 update

--- src/main/java/org/salim/filesync/utils/BackupUtils.java
+++ src/main/java/org/salim/filesync/utils/BackupUtils.java
@@ -1,5 +1,11 @@
 package org.salim.filesync.utils;
 
+import java.io.IOException;
+import java.io.InputStream;
+import java.math.BigInteger;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+
 /**
  * @author Admin
  * 本机信息获取
@@ -10,5 +16,19 @@ package org.salim.filesync.utils;
  */
 public class BackupUtils {
 
+    public static String getFileHash (InputStream inputStream) throws IOException, NoSuchAlgorithmException {
+        MessageDigest messageDigest = MessageDigest.getInstance("MD5");
+
+        byte[] buffer = new byte[1024];
+        int length = -1;
+        while ((length = inputStream.read(buffer, 0, 1024)) != -1) {
+            messageDigest.update(buffer, 0, length);
+        }
+        inputStream.close();
+
+        byte[] md5Bytes = messageDigest.digest();
+        BigInteger result = new BigInteger(1, md5Bytes);
+        return result.toString(16);
+    }
 
 }
